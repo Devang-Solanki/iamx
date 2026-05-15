5fa4652be935aa885579816dcd9cb92f527150ce
Devang Solanki <devangsolanki7718@gmail.com>
2026-05-15 13:59:57 +0000

    chore: bump version to 1.5.0

--- iamx/__init__.py
+++ iamx/__init__.py
@@ -4,5 +4,5 @@ IAMX - Cloud IAM Permission Enumeration Tool
 A CLI tool for enumerating IAM permissions on AWS and GCP cloud platforms.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.5.0"
 __author__ = "Devang Solanki"
